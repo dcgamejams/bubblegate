82525e141589988e0c58c68de3f804864c9db281
Caitlin Lindgren <caitlin.lindgren8@gmail.com>
2025-01-25 11:45:12 -0500

    UpdatedMap

--- assets/scripts/Map.gd
+++ assets/scripts/Map.gd
@@ -12,5 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_treasure_body_entered(body: Node2D) -> void:
-	$Terminal.text = 'YOU HIT TREASURE'
+	$Terminal.text = 'picture of atlantis'
 	pass # Replace with function body.
